1d1e4494cd57c247268a681ba9411f38666f173d
Romil <romil.m.kothari@gmail.com>
2023-03-14 13:42:04 -0400

    tests completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.round(Math.random() * 100);
+ let num3 = 31
+ let num4 = 9
+
+ let mod = num3 % num4;
+
+ let num5 = 20
+ let max = Math.max(1, 5, 7, 10, 20);
